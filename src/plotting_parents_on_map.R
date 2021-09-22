0dc54c628c66e76ff498c590162194ad02f08720
PietrH <48065851+PietrH@users.noreply.github.com>
2021-09-22 16:55:37 +0200

    only wrap the taxon tiles in a function for now

--- src/plotting_parents_on_map.R
+++ src/plotting_parents_on_map.R
@@ -11,10 +11,11 @@ library(mapview)
 
 
 
-parent_a <- hybrids_and_parents[6,"usageKey_a"]
-parent_b <- hybrids_and_parents[6,"usageKey_b"]
+parent_a <- hybrids_and_parents[6,"usageKey_a"] %>% as.integer()
+parent_b <- hybrids_and_parents[6,"usageKey_b"] %>% as.integer()
 
-hybrid <- hybrids_and_parents[6,"hybrid_formula"] %>% rgbif::name_backbone() %>% pull(usageKey)
+hybrid <-
+  hybrids_and_parents[6, "hybrid_formula"] %>% rgbif::name_backbone() %>% pull(usageKey) %>% as.integer()
 
 
 # make a plot -------------------------------------------------------------
@@ -30,7 +31,7 @@ rgbif::map_fetch(source="density",taxonKey=parent_a, style = "gbif-violet") %>% 
 
 
 
-taxon_key = parent_a <- hybrids_and_parents[6,"usageKey_a"] %>% as.integer
+taxon_key = parent_a <- hybrids_and_parents[6,"usageKey_a"]
 
 map_taxon <- function(taxon_key) {
   
@@ -52,3 +53,28 @@ leaflet() %>%
   addTiles(urlTemplate = tilePolygons)  
 
 }
+
+
+# styles available include classic.point purpleYellow.point fire.point
+# glacier.point purpleHeat.point and more, see
+# https://www.gbif.org/developer/maps
+
+create_taxon_tiles <- function(taxon_key,style=sample(c("classic.point","purpleYellow.point","fire.point","glacier.point"),1)) {
+  prefix = 'https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@1x.png?'
+  polygons = sprintf('style=%s',style) # ploygon styles 
+  taxonKey_query = sprintf('taxonKey=%i',taxon_key) 
+  tilePolygons = paste0(prefix,polygons,'&',taxonKey_query)
+  
+  return(addTiles(urlTemplate = tilePolygons))
+}
+
+
+# create leaflet ----------------------------------------------------------
+projection = '3857' # projection code
+style = 'style=osm-bright' # map style
+tileRaster = paste0('https://tile.gbif.org/',projection,'/omt/{z}/{x}/{y}@1x.png?',style)
+
+leaflet() %>%
+  setView(lng = 5.4265362, lat = 43.4200248, zoom = 01) %>%
+  addTiles(urlTemplate = tileRaster) %>%
+  create_taxon_tiles()
