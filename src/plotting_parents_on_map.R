b92deaea4a0263e5e360e1bbfd6e17349f199e2a
PietrH <48065851+PietrH@users.noreply.github.com>
2021-09-22 16:46:30 +0200

    adapting leaflet code, wrap in function to map single taxon

--- src/plotting_parents_on_map.R
+++ src/plotting_parents_on_map.R
@@ -3,7 +3,8 @@
 
 # load libraries ----------------------------------------------------------
 library(leaflet)
-
+library(raster)
+library(mapview)
 
 # set an example ----------------------------------------------------------
 
@@ -27,6 +28,14 @@ rgbif::map_fetch(source="density",taxonKey=parent_a, style = "gbif-violet") %>% 
 # using leaflet -----------------------------------------------------------
 # https://data-blog.gbif.org/post/gbif-maps-api-using-r-and-leaflet/
 
+
+
+taxon_key = parent_a <- hybrids_and_parents[6,"usageKey_a"] %>% as.integer
+
+map_taxon <- function(taxon_key) {
+  
+
+
 # create style raster layer 
 projection = '3857' # projection code
 style = 'style=osm-bright' # map style
@@ -34,11 +43,12 @@ tileRaster = paste0('https://tile.gbif.org/',projection,'/omt/{z}/{x}/{y}@1x.png
 # create our polygons layer 
 prefix = 'https://api.gbif.org/v2/map/occurrence/density/{z}/{x}/{y}@1x.png?'
 polygons = 'style=fire.point' # ploygon styles 
-taxonKey = 'taxonKey=789' # taxonKey of Odonata (dragonflies and damselflies)
-tilePolygons = paste0(prefix,polygons,'&',taxonKey)
+taxonKey_query = sprintf('taxonKey=%i',taxon_key) # taxonKey of Odonata (dragonflies and damselflies)
+tilePolygons = paste0(prefix,polygons,'&',taxonKey_query)
 # plot the styled map
 leaflet() %>%
   setView(lng = 5.4265362, lat = 43.4200248, zoom = 01) %>%
-  addTiles(urlTemplate=tileRaster) %>%
-  addTiles(urlTemplate=tilePolygons)  
+  addTiles(urlTemplate = tileRaster) %>%
+  addTiles(urlTemplate = tilePolygons)  
 
+}
